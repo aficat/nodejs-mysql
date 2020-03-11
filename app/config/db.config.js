8c0b26e4914bda36d69621a18cf0f7624635c427
aficat <aficatrashid@gmail.com>
2020-03-11 15:52:26 +0800

    updated db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "testdb"
+  HOST: "us-cdbr-iron-east-04.cleardb.net",
+  USER: "b7d77b0529b016",
+  PASSWORD: "39bb4b19",
+  DB: "heroku_8bba004bde86914"
 };
